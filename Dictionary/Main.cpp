b8b16240fa2e3bc735121f6c9e3cb5a4c43ae0ef
Brittnie <brittniesexton95@gmail.com>
2022-02-18 11:12:37 -0600

    Tested everything in main

--- Dictionary/Main.cpp
+++ Dictionary/Main.cpp
@@ -45,18 +45,50 @@ void main()
 	
 	inventory[1];*/
 
-	Dictionary<int, const char*>* inventory =  new Dictionary<int, const char*>();
+	Dictionary<int, const char*> inventory =  Dictionary<int, const char*>();
 	
-	inventory->addItem(2, "Two");
-	inventory->addItem(1, "One");
-	inventory->addItem(3, "Three");
-	inventory->addItem(4, "Four");
-	const char* test;
-	//inventory.remove(2);
-	if (inventory->containsKey(5))
+	//Testing addItem Function
+	std::cout << "Testing addItem Function:" << std::endl;
+	inventory.addItem(2, "Two");
+	inventory.addItem(1, "One");
+	inventory.addItem(3, "Three");
+	inventory.addItem(4, "Four");
+	print(inventory);
+	std::cout << "" << std::endl;
+
+	//Testing the remove Function
+	std::cout << "Testing the remove Function: " << std::endl;
+	inventory.remove(3);
+	inventory.remove(4);
+	print(inventory);
+	std::cout << "" << std::endl;
+
+	//Testing the remove that returns a value Funciton
+	std::cout << "Testing the remove function that returns a value: " << std::endl;
+	const char* value;
+	inventory.remove(2, value);
+	std::cout << "The value returned: " << value << std::endl;
+	std::cout << " " << std::endl;
+
+	//Testing the getCount Function
+	std::cout << "Testing the getCount Funciton: " << std::endl;
+	std::cout << inventory.getCount() << std::endl;
+	std::cout << "" << std::endl;
+
+	//Testing the containsKey Function and containsValue Function
+	std::cout << "Testing the containsKey and the containsValue Function: " << std::endl;
+	if (inventory.containsKey(1))
 		std::cout << "Yay" << std::endl;
-	Dictionary<int, const char*> inventory2 = *inventory;
-	delete inventory;
+	std::cout << " " << std::endl;
+
+	if (inventory.containsValue("One"))
+		std::cout << "Yay" << std::endl;
+	std::cout << "" << std::endl;
+
+	//Testing the equal operator
+	std::cout << "Testing the equals operator: " << std::endl;
+	Dictionary<int, const char*> inventory2 = inventory;
+	//delete inventory;
 	
 	
 	print(inventory2);
